47738438ee6c093c225f902f86a5a7321b8fd144
come.willot <come.willot@thinxtra.com>
2019-01-18 00:01:38 +1100

    Fix missing placeholder

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -1,8 +1,8 @@
 <template>
   <v-card :key="index" color="transparent" @click.native="closeDialog()">
     <v-img :style="{'min-height': '200px'}" :contain="true" :src="src" max-width="100%" max-height="100%" min-height="300px">
-      <v-layout align-center justify-center row fill-height>
-        <v-progress-circular :style="{'z-index':'-2'}" :size="80" :width="7" indeterminate color="accent"/>
+      <v-layout slot="placeholder" align-center justify-center ma-0 fill-height>
+        <v-progress-circular :size="80" :width="7" indeterminate color="accent"/>
       </v-layout>
     </v-img>
   </v-card>
